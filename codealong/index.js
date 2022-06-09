fb28839f001cbe7032507c2f553ee3631d46eea9
Robert Armstrong <robert@code-crew.org>
2022-06-09 10:50:58 -0500

    added final comment

--- codealong/index.js
+++ codealong/index.js
@@ -1,10 +1,12 @@
 // make a classroom ?
 'use strict';
 
+// we are going to need a classroom
 
 // we are going to need students
 
-// we are going to need a classroom
-
 // we will need a way to add the students to the classroom
+
+// once the students are in the classroom, each student is going to greet the class
+
 
